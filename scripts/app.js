d95eb7f3e83d0d6325952e9d781a31e69327ca24
Shangni <shangnihu@gmail.com>
2018-08-13 11:26:04 -0400

    Started buildling functionality of tiles and app

---
+++ scripts/app.js
@@ -0,0 +1,131 @@
+const fineApp = {};
+
+// gridSize times gridSized board
+fineApp.gridSize = 4;
+fineApp.score = 0;
+
+fineApp.tile = {
+    x: null,
+    y: null,
+    content: 2,
+
+    prevPosition: null,
+    mergedFrom: null,
+    // saves previous position
+    savePosition() {
+        this.prevPosition = {
+            x: this.x,
+            y: this.y
+        }
+    },
+
+    // updates position
+    updatePosition(position) {
+        this.x = position.x;
+        this.y = position.y;
+    }
+}
+
+// creating a grid that we will push on to the real board with
+fineApp.grid = {
+    size: fineApp.gridSize,
+    cells: [],
+    // buildling grid cells
+    buildGrid() {
+        for (let i = 0; i < fineApp.grid.size; i++) {
+            // create empty rows for each row in cell
+            fineApp.grid.cells[i] = [];
+
+            for (let j = 0; j < fineApp.grid.size; j++) {
+                // fill each row with column number of null spaces
+                fineApp.grid.cells[i].push(null);
+            }
+        }
+    },// results in: an empty board of nulls
+    // [
+    // [null, null, null, null],
+    // [null, null, null, null],
+    // [null, null, null, null],
+    // [null, null, null, null]
+    // ]
+
+    // returns array of objects with [row][col] index
+    // of all positions on grid that are empty (or null)
+    emptyCells() {
+        let cells = [];
+        // loops each row
+        for (let i = 0; i < fineApp.grid.size; i++) {
+            // loops each item in row
+            for (let j = 0; j < fineApp.grid.size; j++) {
+                if (!fineApp.grid.cells[i][j]) {
+                    // pushes index of empty cell into cells array
+                    cells.push({
+                        x: i,
+                        y: j,
+                    });
+                }
+            }
+        }
+        // array of objects with [row][column] indexing for all cells that are still null
+        return cells;
+    },
+
+    // returns  a random empty cell
+    // if there is any empty cells
+    randomEmptyCell() {
+        let cells = fineApp.grid.emptyCells();
+        // if there are empty cells, return a random one
+        if (cells.length) {
+            return cells[Math.floor(Math.random() * cells.length)];
+        }
+    },
+
+    pushTile(tile) {
+        this.cells[tile.x][tile.y] = tile;
+    }
+};
+
+
+// returns true if there are empty cells available
+fineApp.thereAreEmptyCells = () => {
+    return !!fineApp.emptyCells().length;
+}
+
+fineApp.addNewTile = () => {
+
+}
+
+fineApp.addStartTiles = () => {
+    for (let i = 0; i < 2; i ++) {
+
+    }
+}
+
+fineApp.addRandomTile = () => {
+    if (fineApp.emptyCells) {}
+}
+
+
+
+// initialize
+fineApp.init = () => {
+    // reset game
+    fineApp.score = 0;
+    fineApp.gameOver = false;
+    fineApp.gameWon = false;
+
+    // build empty grid
+    fineApp.grid.buildGrid();
+    console.log(`building grid`);
+
+    // add starting tiles to board
+    fineApp.addStartTiles();
+    console.log("start/reseting game");
+}
+
+
+$(function() {
+    console.log("ready");
+    fineApp.init();
+
+})
